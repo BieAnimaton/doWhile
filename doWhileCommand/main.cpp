24ba9e7bb269a9c7e850e7c350ea461d96a435fb
Bie <52220244+BieAnimaton@users.noreply.github.com>
2019-12-11 07:54:08 -0300

    Add files via upload

---
+++ doWhileCommand/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int valor = 0;
+
+    do{
+        cout << "O valor da sequencia eh: " << valor << endl;
+        valor++;
+    }while(valor <= 10);
+
+    system("pause");
+    return 0;
+}
